5f880abb2f1b064a9bad6c7320a39a2d5ed26d69
alperen-ozkan <alperenozkn522@gmail.com>
2025-02-04 15:48:12 +0300

    İlk React proje kodları

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from "./logo.svg"
+function App() {
+  return (
+    <div className="App">
+      <img src="/logo192.png" alt=""></img>
+      <img src={logo} alt=""></img>
+    </div>
+  );
+}
+
+export default App;
